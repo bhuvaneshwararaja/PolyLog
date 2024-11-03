a70bf4da57ddcad1717de0e13728f2e84b4b67c7
bhuvaneshwararaja <bhuvaneshraja113@gmail.com>
2024-11-04 00:07:06 +0530

    chore: updated the folder structure and name

--- polylog/testfiles/test.js
+++ polylog/testfiles/test.js
@@ -1,3 +1,5 @@
 const logStr = "Hello world";
 
+console.error('%c[Log #1730658846430] logStr:', 'color: red; font-weight: bold;', logStr);
+
 console.log('%c[Log #1730657167733] logStr:', 'color: green; font-weight: bold;', logStr);
